f7b8ae8028c64a5cb1523d226eae2ed11c6b3a05
Rahomotul Islam (Shopnil) <shopnilkhan766@gmail.com>
2023-01-23 10:08:25 +0600

    Add files via upload

---
+++ Lab 01/lab3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a,r;
+    cin>>a;
+    string name = "a";
+    for (int i = 0; i < 2; i++)
+    r += a;
+    cout <<r;
+    return 0;}
